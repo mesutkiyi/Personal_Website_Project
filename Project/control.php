24d017f113bea14f500fa068d0e8525f12a86ff8
mesutkiyi <131616380+mesutkiyi@users.noreply.github.com>
2024-05-19 23:34:01 +0300

    Güncelleme 10
    Navbar değiştirildi. Bazı yerlerde grid tasarımına geçildi. İçerik eklendi. Görsel değişiklikler yapıldı...

--- Project/control.php
+++ Project/control.php
@@ -1,9 +1,30 @@
 <?php
+function email_kontrol($email){
+    $regex='/^[a-zA-Z][0-9]{9}@sakarya\.edu\.tr$/';
 
+    return preg_match($regex,$email)===1;
+}
 $kullaniciadi=$_POST["kullaniciadi"];
 $sifre=$_POST["sifre"];
 
 echo"Kullanıcı Adı: ".$kullaniciadi;
 echo"Şifre: ".$sifre;
+
+if(!email_kontrol($kullaniciadi)){
+    echo "Geçersiz kullanıcı adı. Kullanıcı adınız b123456789@sakarya.edu.tr şeklinde olmalıdır";
+}
+else{
+    $numara=explode('@',$kullaniciadi)[0];
+
+    if($sifre===$numara){
+        $_SESSION['username']=$numara;
+        header("Location: welcome.php");
+        exit();
+    }
+    else{
+        header("index.html");
+        exit();
+    }
+}
 
 ?>
